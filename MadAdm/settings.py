01437b35d4417a694b1a44f2f03b71c005890dbc
erichris <waldoh@web502.webfaction.com>
2020-04-25 04:34:26 +0000

    asd

--- MadAdm/settings.py
+++ MadAdm/settings.py
@@ -139,6 +139,6 @@ STATICFILES_DIRS = [
 	#'/home/uniars/src/extra_static_media/',
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "")
 
 STATIC_ROOT = '/home/waldoh/webapps/newmadereria/'
